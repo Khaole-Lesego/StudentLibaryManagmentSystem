8e711bc2ba1fcfc7cd6f816f72f0ce0ae3efdaf2
Happy Joseph Van Der Merwe <st10482408@imconnect.edu.za>
2026-07-28 17:34:28 +0200

    Add project files.

---
+++ StudentLibaryManagmentSystem/BookCatergory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentLibaryManagmentSystem
+{
+    internal class BookCatergory
+    {
+        public enum BookCategory
+        {
+            Technology,
+            Science,
+            Literature,
+            History,
+            Other
+        }
+    }
+}
+
---
+++ StudentLibaryManagmentSystem/StudentRecord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentLibaryManagmentSystem
+{ 
+        public record StudentRecord
+        {
+            public string StudentNumber { get; init; }
+            public string Name { get; init; }
+            public string Course { get; init; }
+
+            public StudentRecord(string studentNumber, string fullName, string course)
+            {
+                if (string.IsNullOrWhiteSpace(studentNumber))
+                    throw new ArgumentException("Student number cannot be empty.", nameof(studentNumber));
+
+                if (string.IsNullOrWhiteSpace(fullName))
+                    throw new ArgumentException("Full name cannot be empty.", nameof(fullName));
+
+                if (string.IsNullOrWhiteSpace(course))
+                    throw new ArgumentException("Course cannot be empty.", nameof(course));
+ 
+            }
+        }
+    }
+
+
